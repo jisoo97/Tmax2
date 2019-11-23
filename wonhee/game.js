9ca6221623e251a3ae13b762dc30df9148be22f5
Jisoo Kim <jisoo97@postech.ac.kr>
2019-11-23 21:39:39 +0900

    Add drawNote function By Jisoo

--- wonhee/game.js
+++ wonhee/game.js
@@ -1,13 +1,27 @@
+const NOTE_MARGIN = 2;
+
 //numOfKey는 난이도에 따른 키의 갯수
 //return없음
 function drawNote(numOfKey){
-	var i;
 	var note_div = document.getElementById("note_div");
-	for(i=1; i<=numOfKey; i++)
+	var customizedWidth = computeCustomizedWidth(numOfKey);
+	for(var i=1; i<=numOfKey; i++)
 	{
 		var note= document.createElement('div');
 		note.classList.add("note");
+		note.style.width = customizedWidth + "px";
+		note.style.margin = NOTE_MARGIN + "px";
 		note_div.appendChild(note);
-		
 	}
+}
+
+//numOfKey - Key의 갯수
+//return - note 1개의 넓이
+function computeCustomizedWidth(numOfKey){
+	var note_div = document.querySelector('#note_div');
+	var largeWidth = note_div.offsetWidth;
+	var margin = NOTE_MARGIN;
+	//border가 2px 라서 숫자만 가져옴
+	var border = window.getComputedStyle(note_div).borderWidth[0];
+	return (largeWidth - (2*border))/numOfKey -(2*margin);
 }
