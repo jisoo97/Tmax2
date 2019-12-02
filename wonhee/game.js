95050db50f5bcc9d41f2e3e35974d53881d773ef
Jisoo Kim <jisoo97@postech.ac.kr>
2019-12-02 22:20:22 +0900

    Create notes through time By Jisoo

--- wonhee/game.js
+++ wonhee/game.js
@@ -1,24 +1,88 @@
 const NOTE_MARGIN = 2;
+const NUM_OF_KEY = 4;
+
+/*var pattern = [[1,0,0,0,1],
+[2,1,1,1,1],
+[2.5,0,1,0,0],
+[2.75,1,0,0,1],
+[3.5,0,1,1,1],
+[4.25,1,0,1,0],
+[5,1,1,1,1],
+[6,1,0,0,1],
+[6.75,0,1,0,1],
+[7.25,0,0,0,1],
+[8,1,1,1,1],
+[8.25,1,0,1,0],
+[9.25,0,1,1,0],
+[9.75,1,1,0,0],
+[10.5,1,1,0,0],
+[11,0,1,0,0],
+[11.5,1,0,0,1],
+[12,0,1,1,1],
+[12.5,1,0,1,0],
+[13,1,1,1,1],
+[13.75,1,0,1,0],
+[14.5,1,1,1,0],
+[15.5,1,1,0,1],
+[16.5,0,1,1,0],
+[17.25,1,0,0,1],
+[17.75,1,1,1,1],
+[18.75,1,1,1,1],
+[19.5,0,0,1,1],
+[20.5,0,1,1,0],
+[21.5,0,0,1,0],
+[22,0,0,1,0],
+[22.5,1,0,0,1],
+[23,0,1,1,0],
+[23.25,1,1,0,1],
+[23.75,0,1,0,0]];
+*/
+var pattern = [[0.5,1,0,0,0],[1.0,0,1,0,0],[1.5,0,0,1,0],[3,0,0,0,1],[5,1,1,1,1]];
+var firstNote = [];
+//주기적으로 불리는 함수
+var intervalID = window.setInterval(gameManager, 250);
+
+var time = 0;
+var i=0;
+function gameManager(){
+	if(pattern[i][0] == time){
+		makeNote(NUM_OF_KEY,pattern[i]);
+		moveNote(firstNote);
+		sleep(1000);
+		i++;
+	}
+	time = time+0.25;
+
+  /*
+ 	updateScore() - 점수 올리기
+	*/
+}
+
+function sleep (delay) {
+	setTimeout(function() {
+		deleteNote();
+	}, delay);
+}
+
+//===========================    Note 관련 ================================
+
 
 //numOfKey는 난이도에 따른 키의 갯수
 //return없음
-
-
 function makeNote(numOfKey,noteLine){
 	var note_div = document.getElementById("note_div");
 	var customizedWidth = computeCustomizedWidth(numOfKey);
-	for(var i=0; i<numOfKey; i++)
+	for(var i=1; i<=numOfKey; i++)
 	{
 		var note= document.createElement('div');
 		note.style.width = customizedWidth + "px";
 		note.style.margin = NOTE_MARGIN + "px";
 		note_div.appendChild(note);
+		firstNote.push(note);
 		if(noteLine[i])
 			note.classList.add("note");
-		
 		else
 			note.classList.add("note_none");
-
 	}
 }
 
@@ -30,39 +94,16 @@ function computeCustomizedWidth(numOfKey){
 	var margin = NOTE_MARGIN;
 	//border가 2px 라서 숫자만 가져옴
 	var border = window.getComputedStyle(note_div).borderWidth[0];
-	return (largeWidth - (2*border))/numOfKey -(2*margin);
+	return (largeWidth - (2*border))/numOfKey -(2*margin) -1;
 }
 
-//주기적으로 불리는 함수
-var intervalID = window.setInterval(gameManager, 300);
-
-var flag = true;
-function gameManager(){
-	makeKeyPad();
-	if(flag){
-		moveNote();
-		flag = false;
-	}
-	deleteNote();
-	/*
-	moveNote() - note위치 수정 ** 한번만 불려도 될듯
-	deleteNote() - 화면에서 벗어난 note 삭제
-	makeNote() - note 그리기
- 	updateScore() - 점수 올리기
-	updateLife() - 목숨 확인 -> 게임 종료할지 말지 결정
-
-	부가적으로 다른 여러 함수가 필요함
-	*/
-}
-
-//parameter - nope
+//noteLine : note 한 줄의 배열
 //return - nope
 //site_url - https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
-function moveNote(){
-	  //** line 단위와 전체 단위 구분 필요
-		var notes = document.querySelectorAll('.note');
-		for(var i=0; i<notes.length; i++){
-			notes[i].animate(
+function moveNote(noteLine){
+	  console.log("move note");
+		for(var i=0; i<noteLine.length; i++){
+			noteLine[i].animate(
 				[
 					// keyframes
 					{ transform: 'translateY(0px)' },
@@ -75,12 +116,23 @@ function moveNote(){
 		}
 }
 
-//https://flatuicolors.com/palette/defo - pallete
+function deleteNote(){
+	console.log("deleteNote");
+	//var position = $(firstNote[0]).offset();
+	//delete the first headed notes
+	for(var i=0; i<NUM_OF_KEY; i++){
+		firstNote.shift().remove();
+	}
+}
 
+
+//===========================   Key 관련 =================================
 var key4 = ['D','F','J','K'];
 var key6 = ['S','D','F','J','K','L'];
 var key8 = ['A','S','D','F','J','K','L',';'];
 
+//numOfkey : key의 갯수
+//return : nope
 function makeKeyPad(numOfKey){
 	var key_div = document.getElementById("key_div");
 	var customizedWidth = computeCustomizedWidth(numOfKey);
@@ -91,23 +143,15 @@ function makeKeyPad(numOfKey){
 		key.style.width =customizedWidth +"px";
 		key.style.margin = NOTE_MARGIN + "px";
 		key_div.appendChild(key);
-	
-		if(numOfKey == 4){
-			key.innerHTML = key4[i];
-			key.id = key4[i];
-		}
-		else if(numOfKey == 6){
-			key.innerHTML = key6[i];
-			key.id = key6[i];
-		}
-		else if(numOfKey == 8){
-			key.innerHTML = key8[i];
-			key.id = key8[i];
-		}
+
+		var keyArr = eval("key" + numOfKey);
+		key.innerHTML = keyArr[i];
+		key.id = keyArr[i];
 	}
 }
 
-
+//keyCode : 문자의 코드(숫자)
+//letter : 눌린 문자
 function getPressedKey(keyCode){
 	var letter;
 	console.log("keyCode", keyCode);
@@ -133,7 +177,7 @@ function getPressedKey(keyCode){
 		case 76:
 			letter = 'L';
 			break;
-		case 59:
+		case 186:
 			letter = ';';
 			break;
 		default :
@@ -143,22 +187,17 @@ function getPressedKey(keyCode){
 	//A=75; S=93; D=78; F=80; J=84; K=85; L=86; ;=69;
 }
 
+//change key color into red
 function changeKeyColor(keyCode){
 	var pressedKey = getPressedKey(keyCode);
 	var key_div = document.getElementById(pressedKey);
 	key_div.style.backgroundColor = "red";
 }
 
+//change key color back into pink
 function changeKeyColorBack(keyCode){
 	console.log("key up");
 	var pressedKey = getPressedKey(keyCode);
 	var key_div = document.getElementById(pressedKey);
 	key_div.style.backgroundColor = "pink";
 }
-
-function deleteNote(){
-	var notes = document.querySelectorAll('.note');
-	var p = $(notes[0]).offset();
-	console.log(p);
-}
-
